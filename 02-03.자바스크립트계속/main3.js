78058d000da43dfc75c813eff4dc680bd2eb296a
CSJ <tmdwns9630@gmail.com>
2022-09-05 15:15:51 +0900

    foreach & map 의 차이

--- 02-03.자바스크립트계속/main3.js
+++ 02-03.자바스크립트계속/main3.js
@@ -1,10 +1,34 @@
 console.log("라이브 서버 작동중3 오후 수업")
 
-//인자의 디폴트값 설정하기
+//인자의 디폴트값 설정하기, 예외 처리
 
+//디폴트값 설정하기
 // const PartTimeJob = (name, pay=9620, time=4) =>{
 const PartTimeJob = (name, pay, time) =>{
     console.log(`이름 : ${name}`);
+    if(name === "" || name === undefined){
+        console.log("이름을 입력하세요")
+        return;
+    }
+    if(!name){
+        //위의 if문을 한번에 처리한 경우
+        console.log("이름을 입력하세요")
+        return;
+    }
+
+    if(typeof(pay) !== 'number' ){
+        console.log("숫자를 바르게 입력하세요")
+        return;
+    }
+    if(typeof(pay) <= 'number' ||){
+        console.log("숫자를 바르게 입력하세요")
+        return;
+    }
+    if(typeof(pay) !== 'number' ||){
+        console.log("숫자를 바르게 입력하세요")
+        return;
+
+
     console.log(`시급 : ${pay.toLocaleString()}`);
     console.log(`근무시간 : ${time}`);
     console.log(`일급 : ${(pay*time).toLocaleString()}`);
@@ -15,4 +39,13 @@ PartTimeJob("강석원",9620,4);
 PartTimeJob("박종훈",10000,3);
 PartTimeJob("윤승준");
 
+//main3.js:8 Uncaught TypeError: Cannot read properties of undefined (reading 'toLocaleString')
+//디폴트 인자가 없을 때 인자를 생략한 경우 toLocalString이 읽을 값이 ㅇ없어서 uncaugth 오류가 뜬다.
+//ㄴ 예외처리를 해줘야 함.
 
+//1 숫자가 아니거나
+//ㄴ typeof ㅌ !== 'number'
+// 2. 0보다 작거나 같을때
+//3. 최저시급보다 작을 때
+//4. undefined 일 때
+//5. 만약 문자열을 입력하는 경우
--- 02-03.자바스크립트계속/main4.js
+++ 02-03.자바스크립트계속/main4.js
@@ -1,18 +1,36 @@
-console.log("라이브 서버 작동중3 오후 수업")
+console.log("라이브 서버 작동중4 오후 수업2")
 
-//인자의 디폴트값 설정하기
+const call3tunes = (callback) =>{
+    for(let i =0; i<3; ++i){
+        //console.log(callback);
 
-// const PartTimeJob = (name, pay=9620, time=4) =>{
-const PartTimeJob = (name, pay, time) =>{
-    console.log(`이름 : ${name}`);
-    console.log(`시급 : ${pay.toLocaleString()}`);
-    console.log(`근무시간 : ${time}`);
-    console.log(`일급 : ${(pay*time).toLocaleString()}`);
-    console.log("----------------");
+        callback(i);
+    }
 }
 
-PartTimeJob("강석원",9620,4);
-PartTimeJob("박종훈",10000,3);
-PartTimeJob("윤승준");
+const myPrint = (idx) => console.log(`${idx}번째 함수 호출`);
 
+call3tunes(myPrint);
+//함수의 인자로 함수를 쓰면 인자의 함수 내용이 그대로 나온다.
+//인자의 콜백함수를 호출하는걸로 보자.
+
+//foreach와 map의 차이
+//foreach의 리턴값은 void, 없다는거고
+//map은 리턴값이 배열이다. 리턴값이 있다. 
+const arr = [20,40,60,80,100];
+
+//arr.map()
+
+const sunmoonfn = (value, index, arr) => {
+    console.log(`value:${value} / index:${index} / arr:${arr}`);
+    return value*value
+};
+
+const sunmoonfn2 = (value) => {
+    console.log(`value:${value}`);
+};
+const forEachResult = arr.forEach(sunmoonfn2);
+
+const mapResult = arr.map(sunmoonfn);
+console.log(mapResult);
 
